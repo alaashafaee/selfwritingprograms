cec2b96a5af91fecc101af567c1ede007481e353
Alaa <alaash@microsoft.com>
2016-02-23 21:35:52 +0200

    renaming classes

--- SelfWritingPrograms/Program.cs
+++ SelfWritingPrograms/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-class B
+class M1
 {
     static void Main()
     {
         string[] S = {
 "        Console.WriteLine(\"using System;\");",
-"        Console.WriteLine(\"class \" + (isA ? \"B\" : \"A\"));",
+"        Console.WriteLine(\"class \" + (isM1 ? \"M2\" : \"M1\"));",
 "        Console.WriteLine(\"{\");",
 "        Console.WriteLine(\"    static void Main()\");",
 "        Console.WriteLine(\"    {\");",
@@ -18,7 +18,7 @@ class B
 "            Console.WriteLine(\"\\\"{0}\\\",\", escapedLine);",
 "        }",
 "        Console.WriteLine(\"        };\");",
-"        Console.WriteLine(\"        bool isA = \" + (isA ? \"false\" : \"true\") + \";\");",
+"        Console.WriteLine(\"        bool isM1 = \" + (isM1 ? \"false\" : \"true\") + \";\");",
 "",
 "        foreach (string line in S) Console.WriteLine(line);",
 "",
@@ -27,9 +27,9 @@ class B
 "",
 "        Console.ReadKey();",
         };
-        bool isA = false;
+        bool isM1 = true;
         Console.WriteLine("using System;");
-        Console.WriteLine("class " + (isA ? "B" : "A"));
+        Console.WriteLine("class " + (isM1 ? "M2" : "M1"));
         Console.WriteLine("{");
         Console.WriteLine("    static void Main()");
         Console.WriteLine("    {");
@@ -42,7 +42,7 @@ class B
             Console.WriteLine("\"{0}\",", escapedLine);
         }
         Console.WriteLine("        };");
-        Console.WriteLine("        bool isA = " + (isA ? "false" : "true") + ";");
+        Console.WriteLine("        bool isM1 = " + (isM1 ? "false" : "true") + ";");
 
         foreach (string line in S) Console.WriteLine(line);
 
